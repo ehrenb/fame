f95586f14249cc12120ebd8ee54486fb00c8cc90
behrenreich <behrenreich@vencorelabs.com>
2018-10-21 08:40:43 -0700

    test fix to url_download module...workers should be requesting json responses

--- fame/common/utils.py
+++ fame/common/utils.py
@@ -70,7 +70,8 @@ def send_file_to_remote(file, url):
         file = open(file, 'rb')
 
     url = urljoin(fame_config.remote, url)
-    response = requests.post(url, files={'file': file}, headers={'X-API-KEY': fame_config.api_key})
+    response = requests.post(url, files={'file': file}, headers={'X-API-KEY': fame_config.api_key,
+                                                                 'Accept': 'Accept': 'application/json'})
     response.raise_for_status()
 
     file.close()
