f21dc6b16aae783e10bb4c63146daf760db651e1
behrenreich <behrenreich@vencorelabs.com>
2018-10-21 08:43:42 -0700

    fix typo

--- fame/common/utils.py
+++ fame/common/utils.py
@@ -71,7 +71,7 @@ def send_file_to_remote(file, url):
 
     url = urljoin(fame_config.remote, url)
     response = requests.post(url, files={'file': file}, headers={'X-API-KEY': fame_config.api_key,
-                                                                 'Accept': 'Accept': 'application/json'})
+                                                                 'Accept': 'application/json'})
     response.raise_for_status()
 
     file.close()
